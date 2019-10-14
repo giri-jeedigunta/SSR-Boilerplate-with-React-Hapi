4c749ca889003c75c6c14b30dc47b04564ccafbd
Jeedigunta, Giri - Giri <vc.giri.jeedigunta@lowes.com>
2019-10-14 12:39:54 +0530

    fixes

--- src/server/index.js
+++ src/server/index.js
@@ -35,7 +35,7 @@ const init = async () => {
     config: {
       handler: {
         directory: {
-          path: Path.resolve("public")
+          path: Path.resolve("dist")
         }
       }
     }
