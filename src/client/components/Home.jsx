1a5826ffbf7b2e88ffbb284be8ebfebf62f02bd4
koushik sadhukhan <ronn.idia@gmail.com>
2019-10-29 20:19:45 +0530

    feat: updating readme file for better understanding.

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -5,6 +5,7 @@ const Home = () => {
     <>
       <h1>React-SSR Boilerplate</h1>
       <p>This is a minimal SSR boilerplate with ReactJS, NodeJS, HapiJS, Webpack.</p>
+      <p>Made the application newly containarized</p>
       <button onClick={() => console.log("Click Works === Hydrate Works")}>Test Events</button>
 
       <a href="https://github.com/giri-jeedigunta/SSR-Boilerplate-with-React-Hapi">Git Repo</a>
